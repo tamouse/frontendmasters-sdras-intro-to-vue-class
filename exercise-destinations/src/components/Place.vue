b4a959e793e18b35f9686a457eb6b4b0d4afaf54
Tamara Temple <tamouse@gmail.com>
2017-07-14 17:11:45 -0500

    YOLO.2017-07-14T17:11:45

---
+++ exercise-destinations/src/components/Place.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="place">
+            <img :src="img" width="235" height="300"/>
+            <slot></slot>
+            <p>{{ desc }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['img', 'desc']
+    }
+</script>
+
+<style lang="scss" scoped>
+    .place {
+        display: flex;
+        flex-direction: column;
+        width: 280px;
+        height: 320px;
+        justify-content: center;
+        background: white;
+        border: 1px solid #ddd;
+        padding: 20px 20px;
+        margin: 10px;
+        h2 {
+            margin: 0;
+            text-align: center;
+        }
+        img {
+            margin: 10px;
+            align-self: center;
+        }
+    }
+</style>
